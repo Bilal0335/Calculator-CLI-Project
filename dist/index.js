4024dfebfcac1b1f721853005492585ede8e969b
Bilal0335 <bilalhussain42201@gmail.com>
2024-05-27 20:05:13 +0500

    changing

--- dist/index.js
+++ dist/index.js
@@ -1,88 +1,92 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 const log = console.log;
-const sleep = () => {
-    return new Promise((res) => {
-        setTimeout(res, 3000);
-    });
-};
+const sleep = (ms = 3000) => new Promise((res) => setTimeout(res, ms));
 async function welcome() {
+    let rainbowTitle = chalkAnimation.rainbow(`
+\t---------------------------------\n\t   Welcome to Simple Calculator   \n\t---------------------------------`);
     await sleep();
     rainbowTitle.stop();
 }
-let rainbowTitle = chalkAnimation.rainbow(`
-\t---------------------------------\n\t   Welcome to Simple Calcualtor   \n\t---------------------------------`); // Animation starts
 await welcome();
 let playAgain;
 do {
-    const answer = await inquirer.prompt([
-        {
-            message: chalk.black.bold("Enter Your First Number:"),
-            type: "input",
-            name: "firsnum",
-            validate: (value) => {
-                if (value.trim() !== "") {
-                    return true;
-                }
-                else {
-                    return `Please enter account Number`;
-                }
+    try {
+        const answers = await inquirer.prompt([
+            {
+                message: chalk.black.bold("Enter Your First Number:"),
+                type: "input",
+                name: "firstnum",
+                validate: (value) => !isNaN(value) || "Please enter a valid number",
+                filter: (value) => parseFloat(value),
             },
-        },
-        {
-            message: chalk.bgRed.white.bold.italic("Select one of the operator to perform action"),
-            type: "list",
-            name: "operator",
-            choices: [
-                chalk.green("+ Addition"),
-                chalk.yellow("- Subtraction"),
-                chalk.gray("* Multiplication"),
-                chalk.red("/ Division"),
-                chalk.blue("^ Power")
-            ],
-        },
-        {
-            message: chalk.black.bold("Enter Your Second Number:"),
-            type: "number",
-            name: "secondnum",
-            validate: (value) => {
-                if (value.trim() !== "") {
-                    return true;
-                }
-                else {
-                    return `Please enter account Number`;
-                }
+            {
+                message: chalk.bgRed.white.bold.italic("Select one of the operators to perform action"),
+                type: "list",
+                name: "operator",
+                choices: [
+                    { name: chalk.green("+ Addition"), value: "+" },
+                    { name: chalk.yellow("- Subtraction"), value: "-" },
+                    { name: chalk.gray("* Multiplication"), value: "*" },
+                    { name: chalk.red("/ Division"), value: "/" },
+                    { name: chalk.blue("^ Power"), value: "^" }
+                ],
             },
-        },
-    ]);
-    const firsNum = Number(answer.firsNum);
-    const secNum = Number(answer.secondnum);
-    //Condtional Statement
-    if (answer.operator === chalk.green("+ Addition")) {
-        log(`${chalk.green(`Addition:`)} ${chalk.blue(`${firsNum + secNum}`)}`);
+            {
+                message: chalk.black.bold("Enter Your Second Number:"),
+                type: "input",
+                name: "secondnum",
+                validate: (value) => !isNaN(value) || "Please enter a valid number",
+                filter: (value) => parseFloat(value),
+            },
+        ]);
+        const { firstnum, operator, secondnum } = answers;
+        if (isNaN(firstnum) || isNaN(secondnum)) {
+            throw new Error("Invalid number input");
+        }
+        let result;
+        switch (operator) {
+            case "+":
+                result = firstnum + secondnum;
+                log(`${chalk.green("Addition:")} ${chalk.blue(result)}`);
+                break;
+            case "-":
+                result = firstnum - secondnum;
+                log(`${chalk.yellow("Subtraction:")} ${chalk.blue(result)}`);
+                break;
+            case "*":
+                result = firstnum * secondnum;
+                log(`${chalk.blueBright("Multiplication:")} ${chalk.blue(result)}`);
+                break;
+            case "/":
+                if (secondnum === 0) {
+                    throw new Error("Division by zero is not allowed.");
+                }
+                result = (firstnum / secondnum).toFixed(4);
+                log(`${chalk.red("Division:")} ${chalk.blue(result)}`);
+                break;
+            case "^":
+                result = Math.pow(firstnum, secondnum);
+                log(`${chalk.red("Power:")} ${chalk.blue(result)}`);
+                break;
+            default:
+                throw new Error("Invalid operator selected.");
+        }
     }
-    else if (answer.operator === chalk.yellow("- Subtraction")) {
-        log(`${chalk.yellow(`Substraction:`)} ${chalk.blue(`${firsNum - secNum}`)}`);
-    }
-    else if (answer.operator === chalk.gray("* Multiplication")) {
-        log(`${chalk.blueBright(`Multiplication:`)} ${chalk.blue(`${firsNum * secNum}`)}`);
-    }
-    else if (answer.operator === chalk.red("/ Division")) {
-        log(`${chalk.red(`Division:`)} ${chalk.blue(`${(firsNum / secNum).toFixed(4)}`)}`);
-    }
-    else if (answer.operator === chalk.blue("^ Power")) {
-        log(`${chalk.red(`Power:`)} ${chalk.blue(`${Math.pow(firsNum, secNum)}`)}`);
-    }
-    else {
-        log(`${chalk.red(`Please Select Invalid Operator`)}`);
+    catch (error) {
+        log(chalk.red(`Error: ${error.message}`));
     }
     const responses = await inquirer.prompt([
         {
             message: "Do you want to continue (Y/N)?",
             type: "input",
             name: "continue",
+            validate: (value) => {
+                const upperValue = value.toUpperCase();
+                return upperValue === "Y" || upperValue === "N" || "Please enter Y or N";
+            },
         },
     ]);
     playAgain = responses.continue.toUpperCase();
